219cee350e4d238bd384b4a594730960355c6f73
ArnelaOmbasa <116111226+ArnelaOmbasa@users.noreply.github.com>
2022-10-25 20:28:46 +0200

    Add files via upload

---
+++ week2/exercise1.java
@@ -0,0 +1,21 @@
+package week2;
+
+import java.util.Scanner;
+
+public class exercise1 {
+    public static void main(String[] args) {
+        String password = "amazing123";
+        while (true) {
+            System.out.print("Type the password: ");
+            String enteredPassword = new Scanner(System.in).nextLine();
+            if (password.equals(enteredPassword)) {
+                System.out.println("Right!");
+                break;
+            } else {
+                System.err.println("Wrong!");
+            }
+        }
+    }
+}
+    
+
---
+++ week2/exercise6.java
@@ -0,0 +1,13 @@
+package week2;
+
+import java.util.Scanner;
+
+public class exercise6 {
+    public static void printText(int timesOfPrinting){
+        for(int i = 0; i < timesOfPrinting; i++) System.out.println("In the beginning there were the swamp, the hoe and Java.");
+    }
+    public static void main(String[] args) {
+        System.out.print("Enter the number for the message to be printed: ");
+        printText(new Scanner(System.in).nextInt());
+    }
+}
